76641101c71aace0e0e6953936fbde6e4ba6c5f2
Marco Goedert <marco.goedert@edu.pucrs.br>
2020-10-19 10:55:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trabalho 1
+
+Plano de execução
+- Jogo pede nickname do jogador
+- Jogador entra no server e aguarda o outro jogador
+- O primeiro jogador da fila começa o jogo
+- O jogador tira o número no dado e avança N casas
+    - Se a casa for neutra, passa a vez
+    - Se a casa for especial positiva, avança 2 casas
+    - Se a casa for especial negativa, volta 2 casas
+- O jogador que chegar no final do tabuleiro primeiro vence
---
+++ server.py
@@ -0,0 +1,59 @@
+import socket
+from _thread import *
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server = '192.168.25.5'
+port = 5556
+
+server_ip = socket.gethostbyname(server)
+
+try:
+    s.bind((server, port))
+
+except socket.error as e:
+    print(str(e))
+
+s.listen(2)
+print("Aguardando todos sobreviventes...")
+
+currentId = "0"
+pos = ["0:100,50", "1:200,100"]
+
+def threaded_client(conn):
+    global currentId, pos
+    conn.send(str.encode(currentId))
+    currentId = "1"
+    reply = ''
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode('utf-8')
+            if not data:
+                conn.send(str.encode("Adeus!"))
+                break
+            else:
+                #print("Recebido: " + reply)
+                arr = reply.split(":")
+                id = int(arr[0])
+                pos[id] = reply
+
+                if id == 0: nid = 1
+                if id == 1: nid = 0
+
+                reply = pos[nid][:]
+                #print("Enviando: " + reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Conexão de", currentId ,"encerrada.")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Conectado", currentId, "em: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
